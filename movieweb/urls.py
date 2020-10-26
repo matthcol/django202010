3901fbf9436c52c0f593d33020856b1177276c70
Matthias Colin <matthias.colin@gmail.com>
2020-10-26 13:03:24 +0100

    end day1:morning

--- movieweb/urls.py
+++ movieweb/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<str:title>/', views.by_title, name='by_title'),
     path('<int:movie_id>/', views.detail, name='detail'),
-    path('by_year/<int:year>/', views.by_year, name='by_year'),
+    path('star/', views.star, name='star')
+#    path('<str:title>/', views.by_title, name='by_title'),
+#    path('by_year/<int:year>/', views.by_year, name='by_year'),
 ]
--- movieweb/views.py
+++ movieweb/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import date
 
 
 def index(request):
-    return render(request, 'movieweb/index.html', {'nbMovies':150})
+    movies = [ 
+            {'title': 'Mulan', 'year':2020, 'duration': 75}, 
+            {'title':'No Time To Die', 'year':2021}, 
+            {'title':'Back To The Future', 'year': 1985}, 
+            {'title':'No Time To Die 3', 'year':2021}, 
+            {'title':'No Time To Die 4', 'year':2021}, 
+            ]
+    return render(request, 'movieweb/index.html', {'movies': movies})
 
 def detail(request, movie_id):
-    render(request, 'movieweb/movie.html', {'movie_id': movie_id})
+    return render(request, 'movieweb/movie.html', {'movie_id': movie_id})
+
+def star(request):
+    stars = [
+            {'name': 'Michael J. Fox', 'birthdate': date(1961,6,9)},
+            {'name': 'Christopher Lloyd', 'birthdate': date(1938,10,22)},
+            ]
+    return render(request, 'movieweb/star.html', {'stars':stars})
 
 def by_year(request, year):
     return HttpResponse(f"Movies from year: {year}")
