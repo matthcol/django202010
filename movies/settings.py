85db4e1780ff3e30569fcf13777c0b5956886ec8
Matthias Colin <matthias.colin@gmail.com>
2020-10-27 09:06:44 +0100

    pg config + notebook query

--- movies/settings.py
+++ movies/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -75,9 +76,17 @@ WSGI_APPLICATION = 'movies.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'sqlite_dev': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dbmovie',
+        'USER': 'movie',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',    
     }
 }
 
